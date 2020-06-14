14d163b7f13c12b9ee05b389245e2334939739b5
Thiago Alves <thiagoalvesda0silva@gmail.com>
2020-06-13 23:42:45 -0600

    added the port variable to work with heroku

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require ('express');
 const app = express();
-
+const PORT = process.env.PORT || 7000;
 const publicRoutes = require('./controllers/publicRoutes.js');
 
 app.set('view engine', 'ejs')
    .use('/', publicRoutes)
-   .listen(7000);
+   .listen(PORT);
